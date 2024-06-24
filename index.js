c12b236495fb2603e4542a909363b670997a3cab
darkapplepower <chictaehee@naver.com>
2024-06-24 19:01:54 +0900

    fix

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const iconv = require("iconv-lite");
-module.exports= class Comci{
-    static async getTimeTable(schoolId, grade, cl, nextweek=false) {
-        nextweek=1+ +!!nextweek;
+module.exports = class Comci {
+    static async getTimeTable(schoolId, grade, cl, nextweek = false) {
+        nextweek = 1 + +!!nextweek;
         var scdata = await this.#scData();
-        var base64 = Buffer.from(scdata + schoolId + "_0_"+nextweek, "utf-8").toString("base64");
+        var base64 = Buffer.from(scdata + schoolId + "_0_" + nextweek, "utf-8").toString("base64");
         var url = await this.#getUrl();
-        var data = await axios.get("http://comci.kr:4082" + url.split("?")[0] + "?" + base64);
+        var data = await axios.get("http://comci.net:4082" + url.split("?")[0] + "?" + base64);
         data = JSON.parse(data.data.replace(/\0/g, ""));
         var zaryo = await this.#searchVariableName();
         var result = {};
         result.수업시간 = JSON.parse(JSON.stringify(data.일과시간));
         result.시간표 = [[], [], [], [], [], []];
-        var ord, dad, th, sb, na;
+        var ord, dad, th, sb, na, tt, ttt;
         console.log();
         for (let t = 1; t < 9; t++) {
-            for (let we = 1; we < 7; we++) {
-                ord = data[zaryo[0]][grade][cl][we][t]||0;
-                dad = data[zaryo[1]][grade][cl][we][t]||0;
-                th = Math.floor(dad / 100);
-                sb = dad - th * 100;
+            for (let we = 1; we < 6; we++) {
+                ord = data[zaryo[0]][grade][cl][we][t] || 0;
+                dad = data[zaryo[1]][grade][cl][we][t] || 0;
                 if (dad > 100) {
+                    th = data.분리 == 100 ? dad / data.분리|0 : dad % data.분리;
+                    sb = data.분리 == 100 ? dad % data.분리 : dad / data.분리|0;
+                    ttt = Math.floor(sb / data.분리);
+                    tt = data.분리 == 100 ? '' : (ttt >= 1 && ttt <= 9) ? String.fromCharCode(t + 64) + "_" : '';
+                    sb = sb % data.분리;
                     if (th < data[zaryo[3]].length) {
                         na = data[zaryo[4]][th].substr(0, 2);
                     } else {
@@ -40,7 +43,7 @@ module.exports= class Comci{
         for (let i = 0; i < schoolName.length; i++) {
             str += "%" + schoolName[i].toString(16).padStart(2, "0").toUpperCase();
         }
-        var data = await axios.get(`http://comci.kr:4082${url}${str}`);
+        var data = await axios.get(`http://comci.net:4082${url}${str}`);
         data = JSON.parse(data.data.replace(/\0/g, ""));
         data = data["학교검색"].map(function (x) {
             return {
@@ -50,13 +53,13 @@ module.exports= class Comci{
         });
         return data;
     }
-    static sortTable(timeTable){
-        return ["월","화","수","목","금","토","일"].map(function(x,xx){
-            if(Array.isArray(timeTable.시간표[xx])){
-                return x+"\n"+timeTable.시간표[xx].map(function(y, yy){
-                    return timeTable.수업시간[yy]+"|"+y;
+    static sortTable(timeTable) {
+        return ["월", "화", "수", "목", "금", "토", "일"].map(function (x, xx) {
+            if (Array.isArray(timeTable.시간표[xx])) {
+                return x + "\n" + timeTable.시간표[xx].map(function (y, yy) {
+                    return timeTable.수업시간[yy] + "|" + y;
                 }).join("\n");
-            }else{
+            } else {
                 return x;
             }
         }).join("\n\n\n");
@@ -85,10 +88,10 @@ module.exports= class Comci{
         return result.slice(result.indexOf("sc_data('") + 8).match(/[^']+/)[0];
     }
     static async #getHtml() {
-        var result = await axios.get("http://comci.kr:4082/st", {
+        var result = await axios.get("http://comci.net:4082/st", {
             responseType: "arraybuffer"
         });
-        result=iconv.decode(result.data, "euc-kr");
+        result = iconv.decode(result.data, "euc-kr");
         result = cheerio.load(result);
         result = result("script")[1].children[0].data;
         return result;
